cc63d52c2d9768f66bd6acb58b0303b4418a125d
Baktiar Miah <baktiar.m@gmail.com>
2019-08-20 20:45:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  
+  while int > 0 
+    def countdown
+  
+    puts "#{int} SECOND(S)!"
+    int-=1
+    if int == 0 
+      return 'HAPPY NEW YEAR!'
+    end
+  end 
 end
